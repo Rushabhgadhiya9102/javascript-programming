855e579d14c1f7eae30f84832ec3b5374c8879c4
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-01-29 23:27:31 +0530

    done

---
+++ day-2/script.js
@@ -0,0 +1,29 @@
+// ------------ DECLARE THE VARIABLE -------------
+
+let age = 22;
+const pi = 3.14;
+let str = "Hello world";
+let bool = true;
+let notAssigned;
+let empty = null;
+let obj = {
+    firstName : "Rushabh",
+    age : 22
+};
+let arr = ["Rushabh", "Rohan", "Rudra"];
+
+// ------------ DECLARE THE VARIABLE -------------
+
+// ------------ PRINT THE TYPE OF VARIABLE -------------
+
+console.log("the type of age Variable : " + typeof age);
+console.log("the type of pi Variable : " + typeof pi);    
+console.log("the type of string Variable : " + typeof str);    
+console.log("the type of boolean Variable : " + typeof bool);    
+console.log("the type of notAssigned Variable : " + typeof notAssigned);    
+console.log("the type of null Variable : " + typeof empty);    
+console.log("the type of obj Variable : " + typeof obj);    
+console.log("the type of arr Variable : " + typeof arr);   
+console.table(arr);
+
+// ------------ PRINT THE TYPE OF VARIABLE -------------
