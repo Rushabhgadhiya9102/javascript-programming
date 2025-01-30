ae8182c54f9ef6e203ec03140ae2737646d82ae6
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-01-30 17:48:12 +0530

    done

--- day-3/grade.js
+++ day-3/grade.js
@@ -3,7 +3,7 @@
 let grade;
 let avg = parseInt(prompt('Enter the Average'));
 
-grade = avg >= 80 ? "A" : avg >= 60 ? "B" : avg >= 50 ? "C" : "Pass";
+grade = avg >= 80 ? "A" : avg >= 60 ? "B" : avg >= 50 ? "C" : avg >= 40 ? "Pass" :"fail";
 
 console.log("Grade is:",grade);
 
---
+++ day-3/strictequal.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = '10';
+
+// -------- STRICT EQUALITY CHECKING ------------
+
+if(a === b){
+    console.log("this is correct");
+} else {
+    console.log("this is incorrect");
+}
+
+// -------- STRICT EQUALITY CHECKING ------------
+
+// -------- STRICT NOT EQUALITY CHECKING ------------
+
+if (a !== b) {
+  console.log("this is valid");
+} else {
+  console.log("this is invalid");
+}
+
+// -------- STRICT NOT EQUALITY CHECKING ------------
