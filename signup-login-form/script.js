9cfbac7cefb35c3a0df566fa23343e5975813161
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-02-06 00:21:58 +0530

    done

--- signup-login-form/script.js
+++ signup-login-form/script.js
@@ -1,11 +1,18 @@
 let signUP = document.querySelector(".sign-up");
 let login = document.querySelector(".login");
+let title = document.querySelector(".title");
+let userCard = document.getElementById("userCard");
 let signUPBtn = document.getElementById("sign-up-btn");
 let loginBtn = document.getElementById("login-btn");
+let btn1 = document.getElementById("btn-login");
+let btn2 = document.getElementById("btn-card");
 
 // -------- BY DEFAULT HIDING ---------
 
 login.style.display = "none";
+userCard.style.display = "none";
+
+// -------- BY DEFAULT HIDING END ---------
 
 signUPBtn.addEventListener("click", () => {
     signUP.style.display = "block";
@@ -13,6 +20,20 @@ signUPBtn.addEventListener("click", () => {
 })
 
 loginBtn.addEventListener("click", () => {
+    
     signUP.style.display = "none";
     login.style.display = "block";
+})
+
+btn1.addEventListener("submit", () => {
+
+    signUP.style.display = "block";
+    login.style.display = "none";
+})
+
+btn2.addEventListener("submit", () => {
+
+    // signUP.style.display = "block";
+    login.style.display = "none";
+    userCard.style.display = "block";
 })
