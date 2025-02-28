33deaf6a98d58087c79d2365de97d1e6755724f2
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-02-28 11:49:51 +0530

    done

--- slider/script.js
+++ slider/script.js
@@ -1,7 +1,14 @@
+// ------------------- V A R I A B L E - D E C L A R A T I O N ----------------
+
 let slidecontainer = document.querySelector(".slider");
 let slides = document.querySelectorAll(".slider-item ");
-
+let prevBtn = document.querySelector(".prev");
+let nextBtn = document.querySelector(".next");
 let currentSlide = 0;
+
+// ------------------- V A R I A B L E - D E C L A R A T I O N ----------------
+
+// ------------------- F U N C T I O N - S H O W - S L I D E S ----------------
 
 function showSlides(index){
 
@@ -16,17 +23,35 @@ function showSlides(index){
 
     }
 
-     slides.forEach((slide, i) => {
+     slides.forEach((slide, index) => {
 
        slide.classList.remove("active");
-       if (i === currentSlide) {
-         slide.classList.add("active");
-
+       if (index === currentSlide) {
+         slide.classList.add("active");    
        }
+
      });
 
 }
 
-function changeSlide(direction){
-    showSlides(currentSlide + direction);
+// ------------------- F U N C T I O N - S H O W - S L I D E S ----------------
+
+// ------------------- F U N C T I O N - C H A N G E - S L I D E S --------------
+
+function changeSlide(n){
+    showSlides(currentSlide + n);
 }
+
+// ------------------- F U N C T I O N - C H A N G E - S L I D E S --------------
+
+// ----------------- N A V I G A T I O N - B U T T O N ------------------
+
+prevBtn.addEventListener("click", () => {
+    changeSlide(-1);
+});
+
+nextBtn.addEventListener("click", () => {
+    changeSlide(1);
+});
+
+// ----------------- N A V I G A T I O N - B U T T O N ------------------
