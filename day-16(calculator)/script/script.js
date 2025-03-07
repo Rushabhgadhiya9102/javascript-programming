725baf5657df669fbe49d50fe943240c19232e11
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-03-07 14:22:43 +0530

    done

--- day-16(calculator)/script/script.js
+++ day-16(calculator)/script/script.js
@@ -3,17 +3,25 @@ const display = document.getElementById("display");
 const operator = ['+','-','*','/'];
 
 button.forEach((btn) => {
+
     btn.addEventListener("click", (e) => {
         let value = e.target.innerText;
 
         if(value === '='){
             display.value = eval(display.value);
+
         }
-        else if(value === 'C'){
+        else if(value === 'AC'){
             display.value = "";
+
+        }
+        else if (value === 'X'){
+            display.value = display.value.slice(0,-1);
+
         }
         else {
             display.value += value;
+
         }
 
     })
