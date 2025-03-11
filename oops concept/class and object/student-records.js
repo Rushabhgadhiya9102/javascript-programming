599cddd1c68fece4a23c2025b72e3c36b9bccfab
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-03-11 19:36:04 +0530

    done

---
+++ oops concept/class and object/student-records.js
@@ -0,0 +1,33 @@
+class Students{
+
+    constructor(id, name, course, average){
+
+        this.id = id;
+        this.name = name;
+        this.course = course;
+        this.average = average;
+
+    }
+
+    details(){
+
+        console.log("Grid: " + this.id);
+        console.log("Name: " + this.name);
+        console.log("Sub_Course: " + this.course);
+        console.log("Average: " + this.average);
+        console.log("  ");
+        
+    }
+
+}
+
+let student1 = new Students(7348, "Rushabh", "C language", "96%");
+let student2 = new Students(7348, "Rudra", "Html/Css", "86%");
+let student3 = new Students(7348, "Rohan", "Bootstrap", "80%");
+let student4 = new Students(7348, "Milan", "Integrated personality development course", "85%");
+
+
+student1.details()
+student2.details()
+student3.details()
+student4.details()
