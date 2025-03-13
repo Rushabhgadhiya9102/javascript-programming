dc18a8f893a515d452149da003675042e0d99f88
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-03-13 17:39:07 +0530

    done

---
+++ oops concept/inheritance/Q1.js
@@ -0,0 +1,24 @@
+class Animal{
+
+    constructor(name){
+        this.name = name;
+    }
+
+    speak(){
+        console.log(`${this.name} makes a noise`);
+        
+    }
+
+}
+
+class Dog extends Animal {
+
+    speak() {
+        console.log(`${this.name} barks`);
+        
+    }
+
+}
+
+const dog = new Dog("rex");
+dog.speak();
