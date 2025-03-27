1637fdcc4e20ac883b6c76eca410a408ac71e2e8
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-03-27 18:08:36 +0530

    done

--- json-form/script.js
+++ json-form/script.js
@@ -2,7 +2,8 @@ let username = document.getElementById("username")
 let email = document.getElementById("email")
 let form = document.getElementById("form")
 let showData = document.querySelector(".table tbody")
-
+let deleteBtn = document.getElementById("deleteBtn");
+let editBtn = document.getElementById("editBtn");
 let users = [];
 username.focus();
 
@@ -32,12 +33,15 @@ function display(){
     users.map((user, index) =>{
         let row = document.createElement("tr");
 
-        row.innerHTML = 
-        `
-        <td>${index+1}</td>
+        row.innerHTML = `
+        <td>${index + 1}</td>
         <td>${user.username}</td>
         <td>${user.email}</td>
-        `
+        <tr>
+            <i id="deleteBtn" class="bi bi-trash3-fill fs-4" onclick="deleteBtn(${index})"></i>
+            <i id="editBtn" class="bi bi-pencil-fill fs-4" onclick="editBtn(${index})"></i>
+        </tr>
+        `;
 
         showData.append(row)
     });
@@ -45,3 +49,16 @@ function display(){
 }
 
 display()
+
+deleteBtn = (index)=>{
+    users.splice(index,1);
+    display()
+}
+
+editBtn = (index)=>{
+
+    let user = users.filter((_,idx) => idx == index)[0]
+    username.value = user.username
+    email.value = user.email
+
+}
